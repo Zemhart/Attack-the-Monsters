6f44ca5efbd158bf36c179c67e23515292b3b490
Zemhart <norulanz@gmail.com>
2020-04-12 23:22:37 +0700

    now monster can change by type

--- models/monster.rb
+++ models/monster.rb
@@ -2,25 +2,35 @@ require 'require_all'
 require_rel "../types"
 
 class Monster
-  def initialize
+ 	attr_reader :hp, :attack, :defense
+  def initialize(type: DefaultType)
     @hp = 30
     @attack = 3
     @defense = 0
+		@type = type.new unless type.nil?
+		set_perks
   end
 
-  def hp
-    @hp
-  end
+	def type
+    @type.nil? ? 'unidentified' : @type.class.name
+	end
 
-  def type; end
+  def name
+    @type.nil? ? 'unidentified' : @type.name
+ 	end
 
-  def attack
-    @attack
-  end
+	def desc
+    @type.nil? ? 'unidentified' : @type.desc
+	end
 
-  def defense
-    @defense
-  end
+	private
 
-  def name; end
+	def set_perks
+    return if @type.nil?
+
+		@type.perks.each do |perk|
+		 var = send(perk[:m_attr])
+		  instance_variable_set("@#{perk[:m_attr]}", var.send(perk[:operator], perk[:value]))
+		end
+	end
 end

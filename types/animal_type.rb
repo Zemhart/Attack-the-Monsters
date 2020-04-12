ebac073d7faa17258ea0da16c3ce4164531abeb2
Zemhart <norulanz@gmail.com>
2020-04-12 23:40:10 +0700

    add animal type monster

---
+++ types/animal_type.rb
@@ -0,0 +1,14 @@
+class AnimalType
+  def name
+    'Animal Monster'
+	end
+
+	def desc
+	 'Corrupted animals turned into monster, retaining their body but also has more dangerous claws and fangs'
+	end
+
+	def perks
+		[{ m_attr: 'hp', operator: '*', value: 1.1 },
+	   { m_attr: 'attack', operator: '+', value: 1 }]
+	end
+end

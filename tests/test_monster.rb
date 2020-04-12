8b585d9019d52d921d4f997e03121e04e4ea234a
Zemhart <norulanz@gmail.com>
2020-04-12 21:35:28 +0700

    add monster spec

---
+++ tests/test_monster.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require_relative '../models/monster'
+
+class TestMonster < Minitest::Test
+  def setup
+    @monster = Monster.new
+  end
+
+  def test_hp_is_30
+		assert_equal 30, @monster.hp    
+  end
+
+	def test_name_is_null
+    assert_nil @monster.name
+  end
+
+  def test_type_is_null
+		assert_nil @monster.type
+	end
+
+  def test_attack_is_3
+		assert_equal 3, @monster.attack
+	end
+
+	def test_defense_is_0
+		assert_equal 0, @monster.defense
+	end 
+end

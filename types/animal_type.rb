9d005c050c6d6b63e00b7ed1798bc80a224f2f10
Zemhart <norulanz@gmail.com>
2020-04-12 23:49:04 +0700

    change spacing

--- types/animal_type.rb
+++ types/animal_type.rb
@@ -1,14 +1,14 @@
 class AnimalType
   def name
     'Animal Monster'
-	end
+  end
 
-	def desc
-	 'Corrupted animals turned into monster, retaining their body but also has more dangerous claws and fangs'
-	end
+  def desc
+    'Corrupted animals turned into monster, retaining their body but also has more dangerous claws and fangs'
+  end
 
-	def perks
-		[{ m_attr: 'hp', operator: '*', value: 1.1 },
-	   { m_attr: 'attack', operator: '+', value: 1 }]
-	end
+  def perks
+    [{ m_attr: 'hp', operator: '*', value: 1.1 },
+    { m_attr: 'attack', operator: '+', value: 1 }]
+  end
 end

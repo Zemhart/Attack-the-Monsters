d91d44965d5e4e0d0ae88ede7efd1927d5806020
Zemhart <norulanz@gmail.com>
2020-04-12 21:35:50 +0700

    add monster class

---
+++ models/monster.rb
@@ -0,0 +1,25 @@
+class Monster
+  def initialize
+    @hp = 30
+    @attack = 3
+    @defense = 0
+  end
+
+  def hp
+    @hp
+  end
+
+  def type; end
+
+  def attack
+    @attack
+  end
+
+  def defense
+    @defense
+  end
+
+  def name; end
+
+  def perks; end
+end
